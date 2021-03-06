0d4c07450d6164de5afdd11bf6a8323617de06de
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2021-03-06 14:50:34 +0200

    Add noexcept specifications for FaceDescriptorComputer methods

--- src/facedescriptorcomputer.h
+++ src/facedescriptorcomputer.h
@@ -26,20 +26,27 @@ public:
 	FaceDescriptorComputer& operator = (FaceDescriptorComputer&& other) = default;*/
 
 
-	std::optional<Descriptor> operator ()(const std::string& file)
+	std::optional<Descriptor> operator ()(const std::string& file) noexcept(
+		std::is_nothrow_invocable_v<FaceExtractor, const std::string&> &&
+		std::is_nothrow_invocable_v<FaceRecognizer, typename FaceExtractor::Output&&> &&
+		// std::optional throws any exception thrown by the constructor of T
+		std::is_nothrow_move_constructible_v<typename FaceExtractor::Output> && 
+		std::is_nothrow_move_constructible_v<Descriptor>)
 	{
 		std::optional<typename FaceExtractor::Output> face = this->faceExtractor(file);
-		return face ? this->faceRecognizer(*face) : std::nullopt;
+		return face ? this->faceRecognizer(*std::move(face)) : std::nullopt;
+		//return face ? this->faceRecognizer(*face) : std::nullopt;
 	}
 
-	std::optional<Descriptor> operator()(const std::filesystem::path& file)
+	// std::filesystem::path::string may throw implementation-defined exceptions
+	std::optional<Descriptor> operator()(const std::filesystem::path& file) 
 	{
 		return (*this)(file.string());
 	}
 
 	std::vector<std::optional<Descriptor>> operator()(const std::vector<std::string>& files, std::size_t maxBatchSize = 64)
 	{
-		std::vector<std::optional<Descriptor>> descriptors(files.size());
+		std::vector<std::optional<Descriptor>> descriptors(files.size());	// may throw
 		auto tail = (*this)(files.cbegin(), files.cend(), descriptors.begin(), maxBatchSize);
 		assert(descriptors.end() == tail);
 		return descriptors;
@@ -47,21 +54,26 @@ public:
 
 	std::vector<std::optional<Descriptor>> operator()(const std::vector<std::filesystem::path>& files, std::size_t maxBatchSize = 64)
 	{
-		std::vector<std::optional<Descriptor>> descriptors(files.size());
+		std::vector<std::optional<Descriptor>> descriptors(files.size());	// may throw
 		auto tail = (*this)(files.cbegin(), files.cend(), descriptors.begin(), maxBatchSize);
 		assert(descriptors.end() == tail);
 		return descriptors;
 	}
 
 	template <class InputIterator, class OutputIterator>
-	OutputIterator operator()(InputIterator inHead, InputIterator inTail, OutputIterator outHead, std::size_t maxBatchSize);
+	OutputIterator operator()(InputIterator inHead, InputIterator inTail, OutputIterator outHead, std::size_t maxBatchSize);	// may throw
 
 protected:
 
+	// Suppress copying, moving, and deletion of descendants through a pointer or reference to this class. It also prevents slicing.
+
+	// There seems to be no guarantee that std::make_from_tuple<> never throws an exception
 	template <typename... FaceExtractorArgs, typename... FaceRecognizerArgs>
 	FaceDescriptorComputer(std::tuple<FaceExtractorArgs...> faceExtractorArgs, std::tuple<FaceRecognizerArgs...> faceRecognizerArgs)
 		: faceExtractor(std::make_from_tuple<FaceExtractor>(std::move(faceExtractorArgs)))
 		, faceRecognizer(std::make_from_tuple<FaceRecognizer>(std::move(faceRecognizerArgs))) {}
+
+	~FaceDescriptorComputer() = default;
 
 	FaceDescriptorComputer(const FaceDescriptorComputer& other) = default;
 	FaceDescriptorComputer(FaceDescriptorComputer && other) = default;
@@ -133,48 +145,6 @@ OutputIterator FaceDescriptorComputer<FaceExtractor, FaceRecognizer>::operator()
 	assert(inHead == inTail);
 	return outHead;
 }	// operator ()
-
-
-
-/*
-// TODO: perhaps, it would be meaningful to rename it to GenericFaceDescriptorComputer<Algorithm, FaceExtractor>
-
-template <class NetworkType, class FaceExtractorType>
-class DnnFaceDescriptorComputer
-{
-	// TODO: check whether the network is callable rather 
-	//static_assert(std::is_convertible_v<typename FaceExtractorType::Output, typename NetworkType::Input>, "FaceExtractor output type must be compatible with Network input type.");
-public:
-	using Network = NetworkType;
-	using FaceExtractor = FaceExtractorType;
-	using Descriptor = typename Network::OutputLabel;
-
-	template <class NetworkT, class FaceExtractorT>
-	DnnFaceDescriptorComputer(NetworkT&& network, FaceExtractorT&& faceExtractor) noexcept(std::is_nothrow_constructible_v<Network, NetworkT>
-																					&& std::is_nothrow_constructible_v<FaceExtractor, FaceExtractorT>)
-		: network(std::forward<NetworkT>(network))
-		, faceExtractor(std::forward<FaceExtractorT>(faceExtractor))
-	{
-
-	}
-
-	// TODO: define copy/move semantics
-
-	template <typename Input>
-	//std::optional<typename Network::Descriptor> compute(Input&& input)
-	std::optional<Descriptor> operator ()(Input&& input)
-	{
-		std::optional<typename FaceExtractor::Output> face = faceExtractor(std::forward<Input>(input));
-		return face ? network(*std::move(face)) : std::nullopt;
-	}
-
-private:
-	FaceExtractor faceExtractor;
-	Network network;
-};	// DnnFaceDescriptorComputer
-
-
-*/
 
 
 
